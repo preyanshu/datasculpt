e6049254d3b2d7cb35d5188d592ddc51c0358d54
Preyanshu Mishra <118627824+preyanshu@users.noreply.github.com>
2024-10-14 17:06:48 +0530

    Update config.js

--- src/context/config.js
+++ src/context/config.js
@@ -1,4 +1,5 @@
 const CONFIG = {
     MODULE_ADDRESS: "0x71be0e1248f94c261bce70c8202ad89d95ef7429837956eb48a0ef9f1c21e662", 
+    NODE_URL: "https://fullnode.devnet.aptoslabs.com"
   };  
   export default CONFIG;
