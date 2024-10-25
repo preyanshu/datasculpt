0dbf8b8536ab95339ce7bdc524fe98741bee8123
Preyanshu Mishra <118627824+preyanshu@users.noreply.github.com>
2024-10-25 08:43:29 +0530

    redeploying to testnet

--- src/context/config.js
+++ src/context/config.js
@@ -1,8 +1,6 @@
 const CONFIG = {
-
-
-    MODULE_ADDRESS: "0x5b33815c334470e3e56d4ba5ba0c4a7c698492748462c416ae0b9b2b4049439f", 
-    NODE_URL: "https://fullnode.devnet.aptoslabs.com"
-  };  
+    MODULE_ADDRESS: "0x5b33815c334470e3e56d4ba5ba0c4a7c698492748462c416ae0b9b2b4049439f",
+    NODE_URL: "https://fullnode.testnet.aptoslabs.com", 
+  };
+  
   export default CONFIG;
-
